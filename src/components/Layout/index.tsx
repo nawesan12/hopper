d59f448081ccab9a43ebf42d3d9380a9b4e6cc0a
Nahuel Santillan <nahueltecnicomdp@gmail.com>
2022-11-28 00:32:55 -0300

    Fixed: Layout instructions

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -3,9 +3,8 @@ import Image from "next/image";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-    <menu>
-
-    </menu>
+      {/*Aca puede haber lo que quieras :) Tanto un menu como un header*/}
+      
       {children}
 
       <footer className="h-5 p-5 flex items-center justify-center">
